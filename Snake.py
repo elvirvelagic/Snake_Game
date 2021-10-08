9ac4abed27e8987abb2343a266fdec44987bd20d
elvir <elvir.1998@hotmail.com>
2021-10-08 15:52:22 +0200

    new test improved code

--- Snake.py
+++ Snake.py
@@ -1,9 +1,10 @@
 import pygame
 import random
-pygame.init()
+
+cell_number = 16
 
 
-class APPLE:
+class Apple:
     def __init__(self):
         self.x = random.randint(0, cell_number - 1)
         self.y = random.randint(0, cell_number - 1)
@@ -14,10 +15,10 @@ class APPLE:
         pygame.draw.rect(screen, (224, 25, 51), apple_rect)
 
 
-class SNAKE:
+class Snake:
     def __init__(self):
         self.body = [pygame.math.Vector2(5, 7), pygame.math.Vector2(6, 7), pygame.math.Vector2(7, 7)]
-        self.direction = pygame.math.Vector2(1, 0)
+        self.direction = pygame.math.Vector2(-1, 0)
 
     def draw_snake(self):
         for block in self.body:
@@ -29,37 +30,42 @@ class SNAKE:
         body_copy.insert(0, body_copy[0] + self.direction)
         self.body = body_copy[:]
 
+def main():
 
-cell_size = 30
-cell_number = 15
-pygame.display.set_caption("Snake")
-screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
-clock = pygame.time.Clock()
+    pygame.init()
+    cell_size = 30
+    cell_number = 16
+    pygame.display.set_caption("Snake")
+    screen = pygame.display.set_mode((cell_size * cell_number, cell_size * cell_number))
+    clock = pygame.time.Clock()
 
-running = True
-apple = APPLE()
-snake = SNAKE()
+    running = True
+    apple = Apple()
+    snake = Snake()
 
-screen_update = pygame.USEREVENT
-pygame.time.set_timer(screen_update, 200)
+    screen_update = pygame.USEREVENT
+    pygame.time.set_timer(screen_update, 200)
 
-while running:
-    for event in pygame.event.get():
-        if event.type == pygame.QUIT:
-            running = False
-        if event.type == screen_update:
-            snake.move_snake()
-        if event.type == pygame.KEYDOWN:
-            if event.key == pygame.K_UP:
-                snake.direction = pygame.math.Vector2(0, -1)
-            if event.key == pygame.K_DOWN:
-                snake.direction = pygame.math.Vector2(0, 1)
-            if event.key == pygame.K_RIGHT:
-                snake.direction = pygame.math.Vector2(1, 0)
-            if event.key == pygame.K_LEFT:
-                snake.direction = pygame.math.Vector2(-1, 0)
-    screen.fill((50, 168, 82))
-    apple.draw_apple()
-    snake.draw_snake()
-    pygame.display.flip()
-    clock.tick(60)
+    while running:
+        for event in pygame.event.get():
+            if event.type == pygame.QUIT:
+                running = False
+            if event.type == screen_update:
+                snake.move_snake()
+            if event.type == pygame.KEYDOWN:
+                if event.key == pygame.K_UP:
+                    snake.direction = pygame.math.Vector2(0, -1)
+                if event.key == pygame.K_DOWN:
+                    snake.direction = pygame.math.Vector2(0, 1)
+                if event.key == pygame.K_RIGHT:
+                    snake.direction = pygame.math.Vector2(1, 0)
+                if event.key == pygame.K_LEFT:
+                    snake.direction = pygame.math.Vector2(-1, 0)
+        screen.fill((50, 168, 82))
+        apple.draw_apple()
+        snake.draw_snake()
+        pygame.display.flip()
+        clock.tick(60)
+
+if __name__ == '__main__':
+    main()
--- Testing.py
+++ Testing.py
@@ -1,1 +1,26 @@
 import unittest
+
+from Snake import Snake, Apple
+
+
+class SnakeTest(unittest.TestCase):
+
+    def test_snake_move(self):
+        snake = Snake()
+        direction = snake.direction
+        start_pos = snake.body.copy()
+        snake.move_snake()
+        start_pos = start_pos[-1:] + start_pos[:-1]
+        start_pos[0] = start_pos[1] + direction
+        moved = [snake.body[i] == start_pos[i] for i in range(len(start_pos))]
+        self.assertTrue(all(moved))
+
+    def test_apple_spawn(self):
+        i = 1
+        while i < 1000:
+            apple = Apple()
+            x = apple.x
+            y = apple.y
+            result = (x + 1) * (y + 1)
+            self.assertLessEqual(result, 256)
+            i += 1
